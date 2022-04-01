f344d3be55c1c9794280abfd558c2529ea915fb7
Hudson Bedward <hudsonbedward@gmail.com>
2022-04-01 13:44:07 -0700

    Adding bunch of buttons and getting the main menu set up and stuff

---
+++ Assets/Hudson/Scripts/HudMainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+//public class HudMainMenu : MonoBehaviour
+//{
+//    public void PlayGame()
+//    {
+//        //or can use SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+//        SceneManager.LoadScene("NathanScene");
+//    }
+
+
+//    public void GoToCreditsMenu()
+//    {
+//        SceneManager.LoadScene("CreditsScene");
+//    }
+
+
+//    public void GoToMainMenu()
+//    {
+//        SceneManager.LoadScene("MenuScene");
+//    }
+
+//    public void QuitGame()
+//    {
+//        Application.Quit();
+//    }
+//}
