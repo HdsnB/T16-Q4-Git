6411721beeea23e2ea66babd7b9d4142f9c5118c
Hudson Bedward <hudsonbedward@gmail.com>
2022-04-01 14:27:04 -0700

    GOT MENU WORKING AND LINKED TO OTHER SCENES

--- Assets/Hudson/Scripts/HudMainMenu.cs
+++ Assets/Hudson/Scripts/HudMainMenu.cs
@@ -1,29 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//public class HudMainMenu : MonoBehaviour
-//{
-//    public void PlayGame()
-//    {
-//        //or can use SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-//        SceneManager.LoadScene("NathanScene");
-//    }
+public class HudMainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //or can use SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); which basically gets the number in the build settings
+        SceneManager.LoadScene("NathanScene");
+    }
 
 
-//    public void GoToCreditsMenu()
-//    {
-//        SceneManager.LoadScene("CreditsScene");
-//    }
+    public void GoToCreditsMenu()
+    {
+        SceneManager.LoadScene("HudCreditsScene");
+    }
 
 
-//    public void GoToMainMenu()
-//    {
-//        SceneManager.LoadScene("MenuScene");
-//    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
 
-//    public void QuitGame()
-//    {
-//        Application.Quit();
-//    }
-//}
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
