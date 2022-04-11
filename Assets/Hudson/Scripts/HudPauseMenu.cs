cc8a123ec7dd27580bd2a258baaf15a2df378e31
MUKILTEO\1710247 <ethanlmcpeek@gmail.com>
2022-04-11 12:53:51 -0700

    PauseMenu

---
+++ Assets/Hudson/Scripts/HudPauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudPauseMenu : MonoBehaviour
+{
+    public bool pausemenu;
+    public GameObject pausemenumenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausemenu == false)
+            {
+                openPausemenu();
+                pausemenu = true;
+            }
+            else if (pausemenu == true)
+            {
+                Closepausemenu();
+                pausemenu = false;
+            }
+
+        }
+    }
+    public void openPausemenu()
+    {
+        Time.timeScale = 0;
+        pausemenumenu.SetActive(true);
+    }
+
+    public void Closepausemenu()
+    {
+        pausemenumenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
