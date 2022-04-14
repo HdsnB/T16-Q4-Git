3e3929a631bb9995fbff53b2913fc3ad4ad23a18
MUKILTEO\1707228 <1707228@mukilteo.wednet.edu>
2022-04-14 13:30:38 -0700

    Enemy Patrol Code started

---
+++ Assets/Joseph/EnemyPatrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<Transform> patrolPts = new List<Transform>();
+    Transform[] patrolPoints = new Transform[] { };
+    public int patrolPath;
+    public float pathTime;
+    public float currentTime;
+    int nPoints;
+    void Start()
+    {
+        patrolPath = 0;
+        patrolPoints = patrolPts.ToArray();
+        nPoints = patrolPoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > pathTime)
+        {
+            currentTime = 0; //reset the timer
+            patrolPath++;
+            if (patrolPath > nPoints - 1) patrolPath = 0;
+        }
+        transform.position = Vector3.Lerp(patrolPoints[patrolPath].position, patrolPoints[(patrolPath + 1) % nPoints].position, currentTime / pathTime);
+    }
+}
