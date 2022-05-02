58445e1a0758e09af51c423c83e0e7365b3aaf5f
MUKILTEO\1605505 <Nrichmond2004@gmail.com>
2022-05-02 12:42:37 -0700

    got oppening cutscene working

---
+++ Assets/NathanR/NathanScripts/NathanCutsceneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NathanCutsceneScript : MonoBehaviour
+{
+    public float FrameDistance;
+    public float creditsMove;
+    public float creditsMoveMax;
+    public string NextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditsMove = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (creditsMove == creditsMoveMax)
+        {
+            SceneManager.LoadScene(NextScene);
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            creditsMove++;
+            transform.position = new Vector3(transform.position.x, transform.position.y - FrameDistance, transform.position.z);
+        }
+    }
+}
