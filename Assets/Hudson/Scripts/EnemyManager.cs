5cb8d3e4a21a85999ea05b6f7a6a426138c6a99b
Hudson Bedward <hudsonbedward@gmail.com>
2022-05-03 13:18:01 -0700

    ENEMY SHOOTING AND MOVEMENT IS ALMOST DONE. BIG PROGRESS HAS BEEN MADE.

---
+++ Assets/Hudson/Scripts/EnemyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject bubba;
+    public float distanceToStopPatrol;
+    public int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(bubba.transform.position.x - this.transform.position.x)  < distanceToStopPatrol  )
+        {
+
+            this.GetComponent<EnemyPatrol>().enabled = false;
+            this.GetComponent<EnemyFire>().enabled = true;
+            if (counter == 1) this.GetComponent<EnemyFire>().CreateFireBall();
+            counter++;
+
+        } else
+        {
+            this.GetComponent<EnemyPatrol>().enabled = true;
+            this.GetComponent<EnemyFire>().enabled = false;
+            counter = 0;
+            
+            
+        }
+    }
+}
--- Assets/Joseph/Scripts/EnemyPatrol.cs
+++ Assets/Joseph/Scripts/EnemyPatrol.cs
@@ -11,12 +11,16 @@ public class EnemyPatrol : MonoBehaviour
     public int patrolPath;
     public float pathTime;
     public float currentTime;
+    SpriteRenderer sr;
     int nPoints;
     void Start()
     {
         patrolPath = 0;
         patrolPoints = patrolPts.ToArray();
         nPoints = patrolPoints.Length;
+        sr = GetComponent<SpriteRenderer>();
+
+
     }
 
     // Update is called once per frame
@@ -28,7 +32,10 @@ public class EnemyPatrol : MonoBehaviour
             currentTime = 0; //reset the timer
             patrolPath++;
             if (patrolPath > nPoints - 1) patrolPath = 0;
+            sr.flipX = !sr.flipX;
+            //have point1 be on left, and point2 be on the right. If changed then this part will flip backwards.
         }
         transform.position = Vector3.Lerp(patrolPoints[patrolPath].position, patrolPoints[(patrolPath + 1) % nPoints].position, currentTime / pathTime);
+        
     }
 }
